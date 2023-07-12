82949cc04ada4398a1afc8da93ef1aee87d2ebaf
BriceNogue <bricenogue@gmail.com>
2023-07-12 02:38:31 +0200

    add header and footer components

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div class="footer">
+        <footer>
+            <h1>Footer</h1>
+        </footer>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Footer',
+}
+</script>
+<style lang="">
+    
+</style>
